0067d201771db7a452d93d948c4015356bd3cd20
Jhalaa Chinoy Tejasvi <jhalaachinoytejasvi@Jhalaas-MacBook-Air.local>
2018-02-12 14:20:42 -0800

    Added generic structure for dfs | jhalaa

--- src/Dfs.java
+++ src/Dfs.java
@@ -2,9 +2,30 @@
 import data.GraphData;
 
 import java.util.List;
+import java.util.Stack;
 
 public class Dfs implements SearchStrategizer {
     public void search(List<GraphData> graph) {
 
+        //Source node -> hard coded right nw. Make it argument later
+        String srcNode = "";
+
+        //Source node -> hard coded right nw. Make it argument later
+        String destNode = "";
+
+        Stack<String> frontier = new Stack<>();
+        frontier.add(srcNode);
+
+        while(!frontier.isEmpty()){
+            String curr = frontier.pop();
+            for(GraphData node: graph){
+                if(node.getFrom().equals(curr)){
+                    if(node.getTo().equals(destNode))
+                        return ;
+                    else
+                        frontier.push(node.getTo());
+                }
+            }
+        }
     }
 }
--- src/data/GraphData.java
+++ src/data/GraphData.java
@@ -1,13 +1,25 @@
 package data;
 
 public class GraphData{
-    String from;
-    String to;
-    int cost;
+    private String from;
+    private String to;
+    private int cost;
 
     public GraphData(String from, String to, int cost){
         this.from = from;
         this.to=to;
         this.cost=cost;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public int getCost() {
+        return cost;
     }
 }
