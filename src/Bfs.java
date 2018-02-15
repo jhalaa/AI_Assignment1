a315e56c6ebe8786a21ca5f2c2ce0ff43a094f9d
Jhalaa Chinoy Tejasvi <jhalaachinoytejasvi@Jhalaas-MacBook-Air.local>
2018-02-15 13:07:31 -0800

    Add bfs - Initial cut

--- src/Bfs.java
+++ src/Bfs.java
@@ -2,25 +2,34 @@ import data.Graph;
 import data.GraphEdges;
 import data.GraphNode;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Bfs implements SearchStrategizer {
     public List<List<GraphEdges>> search(Graph graph, GraphNode src, GraphNode dest) {
+//List of all paths from src to goal node.
+        List<List<GraphEdges>> result = new ArrayList<>();
 
-        //Source node -> hard coded right nw. Make it argument later
-        String srcNode = "";
+        Queue<List<GraphEdges>> frontier = new LinkedList<>();
+        frontier.addAll((Collection<? extends List<GraphEdges>>) graph.getEdges().stream().filter(edge -> edge.getFrom().equals(src)));
 
-        //Source node -> hard coded right nw. Make it argument later
-        String destNode = "";
 
-        Queue<String> frontier = new LinkedList<>();
-        frontier.add(srcNode);
+        while (!frontier.isEmpty()) {
+            List<GraphEdges> curr = frontier.remove();
+            GraphNode lastNode = curr.get(curr.size() - 1).getTo();
+            Set<GraphNode> visited = new HashSet<>();
+            for (GraphEdges edge : graph.getEdges()) {
+                if (edge.getFrom().equals(lastNode) && visited.add(edge.getTo())) {
+                    List<GraphEdges> temp = curr;
+                    temp.add(edge);
+                    if (edge.getTo().equals(dest))
+                        result.add(temp);
+                    else
+                        frontier.add(temp);
+                }
 
-        while (!frontier.isEmpty()){
-            String node = frontier.remove();
-
+            }
         }
+
+        return result;
     }
 }
