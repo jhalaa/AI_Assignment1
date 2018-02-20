df3b97d44c417d51d270d4c92c486e5de3c1aae9
yingchen09 <chenying1109@gmail.com>
2018-02-19 22:21:53 -0800

    add function implementation

--- src/BestFirst.java
+++ src/BestFirst.java
@@ -2,10 +2,56 @@ import data.Graph;
 import data.GraphEdges;
 import data.GraphNode;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
 public class BestFirst implements SearchStrategizer {
-    public List<List<GraphEdges>> search(Graph graph, GraphNode src, GraphNode dest) {
-        return null;
+    public List<List<GraphEdges>> search(Graph graph, GraphNode src, GraphNode dest) throws IllegalArgumentException {
+        List<List<GraphEdges>> result = new ArrayList<>();
+
+        Comparator<List<GraphEdges>> comparator = new Comparator<List<GraphEdges>>() {
+            @Override
+            public int compare(List<GraphEdges> o1, List<GraphEdges> o2) {
+                return Integer.compare(getH(o1, dest),getH(o2, dest));
+            }
+        };
+        Queue<List<GraphEdges>> frontier = new PriorityQueue<List<GraphEdges>>(comparator);
+
+        frontier.addAll((Collection<? extends List<GraphEdges>>)
+                graph.getEdges()
+                        .stream()
+                        .filter(edge -> edge.getFrom()
+                                .equals(src)));
+        if (frontier.isEmpty()) {
+            throw new IllegalArgumentException("Start node is not in the graph!");
+        }
+
+        while (!frontier.isEmpty()) {
+            List<GraphEdges> curr = frontier.poll();
+            GraphNode lastNode = curr.get(curr.size() - 1).getTo();
+            Set<GraphNode> visited = new HashSet<>();
+            for (GraphEdges edge : graph.getEdges()) {
+                if (edge.getFrom().equals(lastNode) && visited.add(edge.getTo())) {
+                    List<GraphEdges> temp = curr;
+                    temp.add(edge);
+                    if (edge.getTo().equals(dest))
+                        result.add(temp);
+                    else
+                        frontier.add(temp);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int getH(List<GraphEdges> list, GraphNode dest) {
+        return HeuristicCreater.HeuristicFunction(list.get(list.size() - 1).getTo(), dest);
     }
 }
--- src/IterativeDeepening.java
+++ src/IterativeDeepening.java
@@ -1,12 +1,53 @@
-
 import data.Graph;
 import data.GraphEdges;
 import data.GraphNode;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 public class IterativeDeepening implements SearchStrategizer {
-    public List<List<GraphEdges>> search(Graph graph, GraphNode src, GraphNode dest) {
-    return null;
+    public List<List<GraphEdges>> search(Graph graph, GraphNode src, GraphNode dest) throws IllegalArgumentException {
+        List<List<GraphEdges>> result = new ArrayList<>();
+        int depth = 0;
+        //return the first path from src to dest
+        while(true) {
+            DLS(graph, src, depth, result, dest);
+            if (!result.isEmpty()) {
+                break;
+            }
+            depth++;
+        }
+        return result;
+    }
+
+    private void DLS(Graph graph, GraphNode src, int depth, List<List<GraphEdges>> result, GraphNode dest) {
+        Stack<List<GraphEdges>> frontier = new Stack<>();
+        frontier.addAll((Collection<? extends List<GraphEdges>>)
+                graph.getEdges()
+                        .stream()
+                        .filter(edge -> edge.getFrom()
+                                .equals(src)));
+        while (!frontier.isEmpty()) {
+            List<GraphEdges> curr = frontier.pop();
+            GraphNode lastNode = curr.get(curr.size() - 1).getTo();
+            Set<GraphNode> visited = new HashSet<>();
+
+            for (GraphEdges edge : graph.getEdges()) {
+                if (edge.getFrom().equals(lastNode) && visited.add(edge.getTo()) && depth > 0) {
+                    List<GraphEdges> temp = curr;
+                    temp.add(edge);
+                    if (edge.getTo().equals(dest)) {
+                        result.add(temp);
+                        return;
+                    }
+                    frontier.push(temp);
+                    depth--;
+                }
+            }
+        }
     }
 }
