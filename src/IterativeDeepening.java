ceebcba430c8e51c39caca102d5989c529287d7b
Jhalaa <jhalaachinoy@gmail.com>
2018-02-25 13:45:02 -0800

    Fixing iterative deepening to prevent stack overflow error

--- src/IterativeDeepening.java
+++ src/IterativeDeepening.java
@@ -6,8 +6,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class IterativeDeepening implements SearchStrategizer {
-    private static final int MAX_BOUND = 8;
-    static int BOUND = 3;
+    static int MAX_BOUND = 20;
+    static int BOUND = 9;
     static int depth = 1;
 
     public List<List<GraphEdges>> search(Graph graph, GraphNode src, GraphNode dest, boolean searchMode) throws IllegalArgumentException {
@@ -15,19 +15,21 @@ public class IterativeDeepening implements SearchStrategizer {
 
         while (depth < BOUND) {
             DLS(graph, src, result, dest, searchMode);
-            if (!result.isEmpty()) {
-                return result;
-            }
             depth++;
         }
-        // if result not found
-        if (depth == BOUND && result.isEmpty()) {
+        if (depth == BOUND && result.isEmpty() && BOUND<MAX_BOUND) {
             System.out.println("Bound is reached and no path is found! Repeat the search by raising the Bound.");
             BOUND++;
-            if(BOUND>MAX_BOUND)
-                throw new IllegalArgumentException("No path Exists!");
             return search(graph, src, dest, searchMode);
         }
+
+        // if result not found
+        if (result.isEmpty())
+            throw new IllegalArgumentException("No path Exists!");
+
+        //if only first solution
+        if(!searchMode)
+            result.subList(1,result.size()).clear();
         return result;
     }
 
@@ -56,34 +58,18 @@ public class IterativeDeepening implements SearchStrategizer {
             throw new IllegalArgumentException("Start or goal node is not in the graph!");
         }
 
-        // if a result of edge length one exists add to frontier
-        if(frontier.stream().anyMatch(list -> list.get(0).getTo().equals(dest))){
-            GraphEdges c = frontier.stream()
-                    .filter(list -> list.get(0).getTo().equals(dest))
-                    .flatMap(Collection::stream)
-                    .findFirst()
-                    .orElse(null);
-            frontier.remove(Arrays.asList(c));
-            result.add(Arrays.asList(c));
-        }
-
-
         int d = depth;
         while (!frontier.isEmpty()) {
             List<GraphEdges> curr = frontier.pop();
             GraphNode lastNode = curr.get(curr.size() - 1).getTo();
             Set<GraphNode> visited = new HashSet<>();
             visited.addAll(MyHelper.getValuesFrom(curr));
-            for (Iterator<GraphEdges> iterator = graph.getEdges().iterator(); iterator.hasNext(); ) {
-                GraphEdges edge = iterator.next();
+            for (GraphEdges edge : graph.getEdges()) {
                 if (edge.getFrom().equals(lastNode) && visited.add(edge.getTo()) && d > 0) {
                     List<GraphEdges> temp = new ArrayList<>(curr);
                     temp.add(edge);
                     if (edge.getTo().equals(dest)) {
                         result.add(temp);
-                        if (!searchMode) {
-                            return;
-                        }
                     }
                     frontier.push(temp);
                     d--;
